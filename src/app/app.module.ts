091bdd9f0bb44b41a89cb1c81a5bcc0ebb28e316
shocko59 <aantoinedw@gmail.com>
2017-07-01 17:24:40 +0200

    Add: homeworks page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { RoundProgressModule, RoundProgressConfig } from 'angular-svg-round-progressbar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HomeworksPage } from '../pages/homeworks/homeworks';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,22 +15,33 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
+    HomeworksPage,
     ListPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    RoundProgressModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
+    HomeworksPage,
     ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    RoundProgressModule, RoundProgressConfig,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private _config: RoundProgressConfig) {
+    _config.setDefaults({
+      color: '#f00',
+      background: '#0f0'
+    });
+  }
+}
---
+++ src/pages/homeworks/homeworks.ts
@@ -0,0 +1,12 @@
+import { Component, NgModule } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-homeworks',
+  templateUrl: 'homeworks.html'
+})
+export class HomeworksPage {
+  constructor(public navCtrl: NavController) {
+
+  }
+}
