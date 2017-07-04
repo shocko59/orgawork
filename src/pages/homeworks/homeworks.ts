20061321fa90e8ee0bc5e61578102a43c7ce7892
shocko59 <aantoinedw@gmail.com>
2017-07-04 18:50:15 +0200

    Add: press event homeworks & notes

--- src/pages/homeworks/homeworks.ts
+++ src/pages/homeworks/homeworks.ts
@@ -1,12 +1,90 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-homeworks',
   templateUrl: 'homeworks.html'
 })
 export class HomeworksPage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController) {
 
+  }
+
+  pressHomework(): void {
+      let actionSheet = this.actionSheetCtrl.create({
+      title: 'Action sur un devoir',
+      buttons: [
+        {
+          text: 'Supprimer',
+          role: 'destructive',
+          handler: () => {
+            console.log('Supression du devoir');
+            this.showConfirm();
+          }
+        },{
+          text: 'Modifier',
+          handler: () => {
+            console.log('Modification du devoir');
+            this.showPrompt();
+          }
+        },{
+          text: 'Annuler',
+          role: 'cancel',
+          handler: () => {
+            console.log('Annulation');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Modification du devoir',
+      inputs: [
+        {
+          name: 'homework',
+          placeholder: 'Nom du devoir'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            console.log('Annulation de la modification');
+          }
+        },
+        {
+          text: 'Enregistrer',
+          handler: data => {
+            console.log('Sauvegarde de la modification');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Suppression du devoir',
+      message: 'Voulez-vous vraiment supprimer ce devoir ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Annulation de la suppression');
+          }
+        },
+        {
+          text: 'Supprimer',
+          handler: () => {
+            console.log('Suppression du devoir');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -1,12 +1,95 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-notes',
   templateUrl: 'notes.html'
 })
 export class NotesPage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController) {
 
+  }
+
+  pressNote(): void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Action sur un devoir',
+      buttons: [
+        {
+          text: 'Supprimer',
+          role: 'destructive',
+          handler: () => {
+            console.log('Supression du devoir');
+            this.showConfirm();
+          }
+        },{
+          text: 'Modifier',
+          handler: () => {
+            console.log('Modification du devoir');
+            this.showPrompt();
+          }
+        },{
+          text: 'Annuler',
+          role: 'cancel',
+          handler: () => {
+            console.log('Annulation');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Modification de la note',
+      inputs: [
+        {
+          name: 'desc',
+          placeholder: 'Description de la note'
+        },
+        {
+          name: 'note',
+          type: 'number',
+          placeholder: 'Note obtenue'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            console.log('Annulation de la modification');
+          }
+        },
+        {
+          text: 'Enregistrer',
+          handler: data => {
+            console.log('Sauvegarde de la modification');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Suppression de la note',
+      message: 'Voulez-vous vraiment supprimer cette note ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Annulation de la suppression');
+          }
+        },
+        {
+          text: 'Supprimer',
+          handler: () => {
+            console.log('Suppression de la note');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
